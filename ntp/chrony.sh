c784a8f70f83fca99b3ebd93418251a22bfd72f5
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-04-07 14:29:19 +0700

    Update chrony.sh

--- ntp/chrony.sh
+++ ntp/chrony.sh
@@ -8,11 +8,34 @@ echo Installing/Checking packages from list
 apt install chrony -y
 sleep 0.5
 
-echo Installing/Checking packages from list
-cp chrony.conf
+if cp /etc/chrony/chrony.conf /etc/chrony/chrony.conf.bak; then
+  echo "Backup file chrony.conf .."
+  sleep 0.5
+else
+  echo "Cant backup chrony.conf .."
+  sleep 0.5
+fi
+
+if rm -rf /etc/chrony/chrony.conf; then
+  echo "Remove chrony.conf .."
+  sleep 0.5
+else
+  echo "Cant remove.."
+  sleep 0.5
+fi
+
+if cp -R chrony.conf /etc/chrony/; then
+  echo "chrony.conf has been copied.."
+  sleep 0.5
+else
+  echo "Cant copied chrony.conf .."
+  sleep 0.5
+fi
+
+echo Restarting chrony...
+systemctl restart chronyd
 sleep 0.5
 
-systemctl restart chronyd
-
-nano /etc/systemd/timesyncd.conf
-systemctl restart systemd-timesyncd
+echo checking time...
+chronyc sources
+sleep 0.5
