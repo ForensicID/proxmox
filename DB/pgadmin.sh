6aef824a38346ef8198b38c6559f626dbdcf7459
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-04-09 06:02:41 +0700

    Create pgadmin.sh

---
+++ DB/pgadmin.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg; then
+  echo "GET Packages from pgadmin .."
+  sleep 1
+else
+  echo "Cant GET Packages from pgadmin .."
+  sleep 0.5
+
+if sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'; then
+  echo "Update Packages .."
+  sleep 1
+else
+  echo "CANT Update Packages .."
+  sleep 0.5
+  
+if apt install pgadmin4 -y; then
+  echo "Install pgadmin .."
+  sleep 1
+else
+  echo "CANT Install pgadmin .."
+  sleep 0.5
+
+if sudo /usr/pgadmin4/bin/setup-web.sh -y; then
+  echo "Setup pgadmin web .."
+  sleep 1
+else
+  echo "CANT Setup pgadmin web .."
+  sleep 0.5
