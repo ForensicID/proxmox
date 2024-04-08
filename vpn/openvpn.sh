4310c58272f9d11c126f1b61d649b25a0868f539
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-04-08 12:27:01 +0700

    Update openvpn.sh

--- vpn/openvpn.sh
+++ vpn/openvpn.sh
@@ -23,7 +23,7 @@ else
   sleep 0.5
 fi
 
-echo Installing/Checking packages from openvpn & easy-rsa
+echo Edit file vars ...
 sed -i "s/#set_var EASYRSA        "${0%/*}"/set_var EASYRSA "$PWD"/g" /etc/openvpn/easy-rsa/vars
 sed -i "s/#set_var EASYRSA_PKI            "$PWD/pki"/set_var EASYRSA_PKI             "$PWD/pki"/g" /etc/openvpn/easy-rsa/vars
 sed -i "s/#set_var EASYRSA_DN     "cn_only"/set_var EASYRSA_DN      "cn_only"/g" /etc/openvpn/easy-rsa/vars
@@ -31,16 +31,18 @@ sed -i "s/#set_var EASYRSA_REQ_COUNTRY    "US"/set_var EASYRSA_REQ_COUNTRY     "
 sed -i "s/#set_var EASYRSA_REQ_PROVINCE   "California"/set_var EASYRSA_REQ_PROVINCE    "DIY"/g" /etc/openvpn/easy-rsa/vars
 sed -i "s/#set_var EASYRSA_REQ_CITY       "San Francisco"/set_var EASYRSA_REQ_CITY        "Kota Yogyakarta"/g" /etc/openvpn/easy-rsa/vars
 sed -i "s/#set_var EASYRSA_REQ_ORG        "Copyleft Certificate Co"/set_var EASYRSA_REQ_ORG         "SMKN 2 Yogyakarta"/g" /etc/openvpn/easy-rsa/vars
-sed -i "s/#set_var EASYRSA_REQ_EMAIL      "me@example.net"/set_var EASYRSA_REQ_EMAIL       "admin@mail.lksjogja25.lan"/g" /etc/openvpn/easy-rsa/vars
-sed -i "s/#set_var EASYRSA_REQ_OU         "My Organizational Unit"/set_var EASYRSA_REQ_OU          "vpn.lksjogja25.lan"/g" /etc/openvpn/easy-rsa/vars
-sed -i "s/ganti/ganti/g" /etc/openvpn/easy-rsa/vars
-sed -i "s/ganti/ganti/g" /etc/openvpn/easy-rsa/vars
-sed -i "s/ganti/ganti/g" /etc/openvpn/easy-rsa/vars
-sed -i "s/ganti/ganti/g" /etc/openvpn/easy-rsa/vars
-sed -i "s/ganti/ganti/g" /etc/openvpn/easy-rsa/vars
-sed -i "s/ganti/ganti/g" /etc/openvpn/easy-rsa/vars
-
-
+sed -i "s/#set_var EASYRSA_REQ_EMAIL      "me@example.net"/set_var EASYRSA_REQ_EMAIL       "admin@mail.$domain"/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_REQ_OU         "My Organizational Unit"/set_var EASYRSA_REQ_OU          "vpn.$domain"/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_KEY_SIZE       2048/set_var EASYRSA_KEY_SIZE        2048/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_ALGO           rsa/set_var EASYRSA_ALGO            rsa/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_CA_EXPIRE      3650/set_var EASYRSA_CA_EXPIRE       3650/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_CERT_EXPIRE    825/set_var EASYRSA_CERT_EXPIRE     825/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_NS_SUPPORT     "no"/set_var EASYRSA_NS_SUPPORT     "no"/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_EXT_DIR        "$EASYRSA/x509-types"/set_var EASYRSA_EXT_DIR "$EASYRSA/x509-types"/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_SSL_CONF       "$EASYRSA/openssl-easyrsa.cnf"/set_var EASYRSA_SSL_CONF        "$EASYRSA/openssl-easyrsa.cnf"/g" /etc/openvpn/easy-rsa/vars
+sed -i "s/#set_var EASYRSA_DIGEST         "sha256"/set_var EASYRSA_DIGEST          "sha256"/g" /etc/openvpn/easy-rsa/vars
+echo Edited successfull ...
+sleep 2
 
 if sed -i "s/#set_var EASYRSA        "${0%/*}"/set_var EASYRSA "$PWD"/g" /etc/openvpn/easy-rsa/vars; then
   echo "named.conf.local has been copied.."
