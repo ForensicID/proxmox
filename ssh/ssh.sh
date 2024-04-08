7107b8c974bb6fd197825b62f941b14b20985303
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-04-08 14:23:39 +0700

    Update ssh.sh

--- ssh/ssh.sh
+++ ssh/ssh.sh
@@ -5,10 +5,21 @@ port="2004"
 
 echo Installing/Checking packages from list
 apt install -y ssh
-sleep 10
+sleep 0.5
 
 echo Change configuration from /etc/ssh/sshd_config
 sed -i "s/#Port 22/Port $port/g" /etc/ssh/sshd_config
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
 sed -i "s/#Banner none/Banner /etc/ssh/banner/g" /etc/ssh/sshd_config
 sleep 0.5
+
+if cp banner /etc/ssh/; then
+  echo "Copying banner ssh .."
+  sleep 0.5
+else
+  echo "Cant Copying banner ssh .."
+  sleep 0.5
+
+echo Restarting ssh...
+systemctl restart ssh
+sleep 2
