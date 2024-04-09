7768252213f2057d973776b1d1f08f3ec9d562ef
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-04-09 09:30:12 +0700

    Update pgadmin.sh

--- DB/pgadmin.sh
+++ DB/pgadmin.sh
@@ -6,6 +6,7 @@ if curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg 
 else
   echo "Cant GET Packages from pgadmin .."
   sleep 0.5
+fi
 
 if sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'; then
   echo "Update Packages .."
@@ -13,13 +14,15 @@ if sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg
 else
   echo "CANT Update Packages .."
   sleep 0.5
-  
+fi
+
 if apt install pgadmin4 -y; then
   echo "Install pgadmin .."
   sleep 1
 else
   echo "CANT Install pgadmin .."
   sleep 0.5
+fi
 
 if sudo /usr/pgadmin4/bin/setup-web.sh -y; then
   echo "Setup pgadmin web .."
@@ -27,3 +30,4 @@ if sudo /usr/pgadmin4/bin/setup-web.sh -y; then
 else
   echo "CANT Setup pgadmin web .."
   sleep 0.5
+fi
