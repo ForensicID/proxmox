50f14cc7fda5ec23a378e2f1340d5a80aedec77d
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-04-22 18:27:32 +0700

    Update user.sh

--- ldap/user.sh
+++ ldap/user.sh
@@ -3,7 +3,7 @@ for i in {1..10};
 do
    if [ $i -eq 10 ];
    then
-        echo "dn: uid=vpnuser10,cn=vpn,ou=VPN,dc=lksjogja1,dc=lan" >> vpnuser.ldif
+        echo "dn: uid=vpnuser10,cn=vpn,ou=VPN,dc=lksjogja14,dc=lan" >> vpnuser.ldif
         echo "objectClass: inetOrgPerson" >> vpnuser.ldif
         echo "objectClass: posixAccount" >> vpnuser.ldif
         echo "objectClass: shadowAccount" >> vpnuser.ldif
@@ -14,13 +14,13 @@ do
         echo "displayName: vpnuser10" >> vpnuser.ldif
         echo "uidNumber: 2010" >> vpnuser.ldif
         echo "gidNumber: 2000" >> vpnuser.ldif
-        echo "userPassword: Skill1" >> vpnuser.ldif
+        echo "userPassword: Skill14" >> vpnuser.ldif
         echo "gecos: vpnuser10" >> vpnuser.ldif
         echo "loginShell: /bin/bash" >> vpnuser.ldif
         echo "homeDirectory: /home/vpnuser$i" >> vpnuser.ldif
         echo "" >> vpnuser.ldif
    else
-        echo "dn: uid=vpnuser$i,cn=vpn,ou=VPN,dc=lksjogja1,dc=lan" >> vpnuser.ldif
+        echo "dn: uid=vpnuser$i,cn=vpn,ou=VPN,dc=lksjogja14,dc=lan" >> vpnuser.ldif
         echo "objectClass: inetOrgPerson" >> vpnuser.ldif
         echo "objectClass: posixAccount" >> vpnuser.ldif
         echo "objectClass: shadowAccount" >> vpnuser.ldif
@@ -31,7 +31,7 @@ do
         echo "displayName: vpnuser$i" >> vpnuser.ldif
         echo "uidNumber: 200$i" >> vpnuser.ldif
         echo "gidNumber: 2000" >> vpnuser.ldif
-        echo "userPassword: Skill1" >> vpnuser.ldif
+        echo "userPassword: Skill14" >> vpnuser.ldif
         echo "gecos: vpnuser$i" >> vpnuser.ldif
         echo "loginShell: /bin/bash" >> vpnuser.ldif
         echo "homeDirectory: /home/vpnuser$i" >> vpnuser.ldif
